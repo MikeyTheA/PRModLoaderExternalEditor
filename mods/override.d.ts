8e99fde8d548f57493373c62bea15d14b025d4b2
MikeyTheA <170099338+MikeyTheA@users.noreply.github.com>
2024-06-23 16:33:01 +0300

    improve overrides

--- mods/override.d.ts
+++ mods/override.d.ts
@@ -1,7 +1,29 @@
 declare const log: (message: String) => void;
 declare const error: (message: String) => void;
 declare const ImGui: any;
-declare const data: any;
 declare const hook: (phase: String, func: Function) => void;
 declare const addWindow: (name: String, show: Function, flags: any) => void;
 declare const getBattleScene: () => PokeRogue.BattleScene;
+
+declare const globalData: {
+    constructor(prefix?: String);
+    getData(key: String, init: any, persistent?: Boolean): any;
+    setData(key: String, value: any, persistent?: Boolean): void;
+    getAccess(key: String, init: any, persistent?: Boolean): (value?: any) => any;
+    addListener(key: String, init: any, listener: Function, persistent?: Boolean, identifier?: String): void;
+}
+
+declare const data: {
+    constructor(prefix?: String);
+    getData(key: String, init: any, persistent?: Boolean): any;
+    setData(key: String, value: any, persistent?: Boolean): void;
+    getAccess(key: String, init: any, persistent?: Boolean): (value?: any) => any;
+    addListener(key: String, init: any, listener: Function, persistent?: Boolean, identifier?: String): void;
+}
+
+declare const getInstalledMods: () => Array<{
+    name: string,
+    version: number,
+    author: string
+}>
+
